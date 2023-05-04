30349edb1a1544b370d05484b4fbee8553d4e726
kooya3 <elyeestatua@gmail.com>
2023-05-04 11:44:29 +0300

    created the github logo btn on the navbar

--- ai-summarizer/src/components/Hero.jsx
+++ ai-summarizer/src/components/Hero.jsx
@@ -5,7 +5,12 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
     <nav className="flex justify-between items-center flex-col">
-      <img />
+      <img src={logo} alt="sam_logo" className="w-28 object-contain" />
+      <button 
+      type="button"
+      onClick={() => window.open('https://github.com/kooya3')}
+      className="black_btn"
+      >Github</button>
     </nav>
     </header>
   )
