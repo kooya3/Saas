cb42bdcda19042aa699518c09ca14ca6e1f8a56e
kooya3 <elyeestatua@gmail.com>
2023-05-06 18:44:16 +0300

    created the paste link input field

--- ai-summarizer/src/components/Demo.jsx
+++ ai-summarizer/src/components/Demo.jsx
@@ -1,67 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { copy, linkIcon, loader, tick } from "../assets";
-/* import { useLazyGetSummaryQuery } from "../services/article";
- */
+
 
 const Demo = () => {
-  const [article, setArticle] = useState({
-    url: "",
-    summary: "",
-
-  });
-
-  const [allArticles, setAllArticles] =useState([]);
-  const [copied, setCopied] = useState("");
-
-  // RTK lazy Query
-/*   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery(); */
-  
-  // Load data from Local Storage on Mount
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("articles")
-    );
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const existingArticle = allArticles.find(
-      (item) => item === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
-    const { data } = await getSummary({ articleUrl: article.url });
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
-
-
-      // update state and local storage
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    }
-  };
-
-  // copy the url and toggle the icon for user feedback
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      handleSubmit(e);
-    }
-  };
-
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+    <div className='flex flex-col w-full gap-2'>
+      <form
+        className='relative flex justify-center items-center'
+        onSubmit={() => {}}
+      >
+        <img
+          src={linkIcon}
+          alt='link-icon'
+          className='absolute left-0 my-2 ml-3 w-5'
+        />
+
+        <input
+          type='url'
+          placeholder='Paste the article Url'
+          value=""
+          onChange={() => {}}
+          /* onKeyDown={handleKeyDown} */
+          required
+          className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+        />
+        <button
+          type='submit'
+          className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+        >
+          <p>↵</p>
+        </button>
+      </form>
+
+         </div>
+  </section>
   )
 }
 
-export default Demo
+export default Demo;
