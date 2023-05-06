b05c0f070156093b8471bb6b122bb8e2e75354cd
kooya3 <elyeestatua@gmail.com>
2023-05-06 09:37:27 +0300

    created the project.

--- ai-summarizer/src/components/Hero.jsx
+++ ai-summarizer/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/kooya3", "_blank")
           }
           className='black_btn'
         >
