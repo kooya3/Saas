f5db0facb1d6f711b2abbb60b1bdebbc90253c79
kooya3 <elyeestatua@gmail.com>
2023-05-06 17:17:39 +0300

    created demo function

--- ai-summarizer/src/components/Demo.jsx
+++ ai-summarizer/src/components/Demo.jsx
@@ -1,6 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
+
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+
+  });
+
+  const [allArticles, setAllArticles] =useState([]);
+  const [copied, setCopied] = useState("");
+  
   return (
     <div>Demo</div>
   )
