7a09e4619c9258808cefb6a3a51e43ab06761086
kooya3 <elyeestatua@gmail.com>
2023-05-06 19:42:53 +0300

    created the Store redux toolkit and env file

--- ai-summarizer/src/main.jsx
+++ ai-summarizer/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { store } from "./services/store";
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store} >
       <App />
     </Provider>
   </React.StrictMode>,
