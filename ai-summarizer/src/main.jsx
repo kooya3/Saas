aaeb5f537b741236652eb207ee0d71cbd006c0e6
kooya3 <elyeestatua@gmail.com>
2023-05-06 18:57:29 +0300

    created the PRovider component from redux

--- ai-summarizer/src/main.jsx
+++ ai-summarizer/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
