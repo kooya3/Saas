4ac160422771df46f6422a711c8325af1ab2db67
kooya3 <elyeestatua@gmail.com>
2023-05-09 11:25:11 +0300

    created the summary article class section

--- ai-summarizer/src/components/Demo.jsx
+++ ai-summarizer/src/components/Demo.jsx
@@ -125,7 +125,16 @@ const Demo = () => {
               {error?.data?.error}
             </span>
           </p>
-        
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
               </div>
             </div>
           )
